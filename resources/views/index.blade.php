c02bf11dccc1abee7dfb5927796f62f401bb1c67
Vincent <vinotiono@gmail.com>
2024-10-22 14:53:25 +0700

    Commit pertama

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.menu')
+@section('konten')
+    <div class="flex justify-evenly items-center p-8 bg-[#FFFAE9] mx-8`">
+    <!-- Card 1 -->
+    <div class="bg-white rounded-2xl shadow-lg overflow-hidden w-64 mb-12">
+        <img src="img/rumah1.jpg" alt="House 1" class="w-full h-auto">
+    </div>
+
+    <!-- Card 2 -->
+    <div class="bg-white rounded-2xl shadow-lg overflow-hidden w-64 mt-12">
+        <img src="img/rumah1.jpg" alt="House 2" class="w-full h-auto">
+    </div>
+
+    <!-- Card 3 -->
+    <div class="bg-white rounded-2xl shadow-lg overflow-hidden w-64 mb-12">
+        <img src="img/rumah1.jpg" alt="House 3" class="w-full h-auto">
+    </div>
+
+    <!-- Card 4 -->
+    <div class="bg-white rounded-2xl shadow-lg overflow-hidden w-64 mt-12">
+        <img src="img/rumah1.jpg" alt="House 4" class="w-full h-auto">
+    </div>
+</div>
+
+@endsection
